00dbe4765a0f58684ce1d728141fc290943157db
Alessio Di Nardo <alessio.dinardo91@gmail.com>
2023-01-18 15:55:16 +0100

    first commit

---
+++ rick-and-morty/rick-and-morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rick-and-morty
+//
+//  Created by Alessio Di Nardo on 18/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
