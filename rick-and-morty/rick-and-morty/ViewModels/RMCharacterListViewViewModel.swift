f8481d7eecf9e8b4754d75df5e2f5050e77d05bb
Alessio Di Nardo <alessio.dinardo91@gmail.com>
2023-01-26 17:09:07 +0100

    Part 8 complete

--- rick-and-morty/rick-and-morty/ViewModels/RMCharacterListViewViewModel.swift
+++ rick-and-morty/rick-and-morty/ViewModels/RMCharacterListViewViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  CharacterListViewViewModel.swift
+//  RMCharacterListViewViewModel.swift
 //  rick-and-morty
 //
 //  Created by Alessio Di Nardo on 26/01/23.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-final class CharacterListViewViewModel: NSObject {
+final class RMCharacterListViewViewModel: NSObject {
     func fetchCharacters() {
         RMService.shared.execute(.listCharactersRequests, expecting: RMGetAllCharactersResponse.self) { result in
             switch result {
             case .success(let model):
-                print("Total:"+String(model.info.count))
-                print("Page result count:"+String(model.results.count))
+                print("Example image url:"+String(model.results.first?.image ?? "No image found"))
             case .failure(let failure):
                 print(String(describing: failure))
             }
@@ -21,19 +20,23 @@ final class CharacterListViewViewModel: NSObject {
     }
 }
 
-extension CharacterListViewViewModel: UICollectionViewDataSource {
+extension RMCharacterListViewViewModel: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 20
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .systemGreen
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RMCharacterCollectionViewCell.cellIdentifier, for: indexPath) as? RMCharacterCollectionViewCell
+        else {
+            return UICollectionViewCell()
+        }
+        let viewModel = RMCharacterCollectionViewCellViewModel(characterName: "Ale", characterStatus: .alive, characterImageUrl: URL(string: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"))
+        cell.configure(with: viewModel)
         return cell
     }
 }
 
-extension CharacterListViewViewModel: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+extension RMCharacterListViewViewModel: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let bounds = UIScreen.main.bounds
         let width = (bounds.width-30)/2
--- rick-and-morty/rick-and-morty/Views/RMCharacterListView.swift
+++ rick-and-morty/rick-and-morty/Views/RMCharacterListView.swift
@@ -1,5 +1,5 @@
 //
-//  CharacterListView.swift
+//  RMCharacterListView.swift
 //  rick-and-morty
 //
 //  Created by Alessio Di Nardo on 26/01/23.
@@ -7,10 +7,10 @@
 
 import UIKit
 
-/// View that handlez showing list of characters, loader, ecc.
-final class CharacterListView: UIView {
+/// View that handler showing list of characters, loader, ecc.
+final class RMCharacterListView: UIView {
     
-    private let viewModel = CharacterListViewViewModel()
+    private let viewModel = RMCharacterListViewViewModel()
     
     private let spinner: UIActivityIndicatorView = {
         let spinner = UIActivityIndicatorView(style: .large)
@@ -27,7 +27,7 @@ final class CharacterListView: UIView {
         collectionView.isHidden = true
         collectionView.alpha = 0
         collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.register(RMCharacterCollectionViewCell.self, forCellWithReuseIdentifier: RMCharacterCollectionViewCell.cellIdentifier)
         return collectionView
     }()
     
